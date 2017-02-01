56e3e39ee4272df08d0af8c2efabfbdf9c58ae2b
Aaron <aaron.long.c@gmail.com>
2017-02-01 00:45:07 -0500

    Added more fonts to Sass

--- js/src/components/App.js
+++ js/src/components/App.js
@@ -5,18 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="card">
-          <header className="card-header">
-            <p className="card-header-title">
-              Component
-            </p>
-            <a className="card-header-icon">
-              <span className="icon">
-                <i className="fa fa-angle-down"></i>
-              </span>
-            </a>
-          </header>
-        </div>
+     
       </div>
     );
   }
