7d048a300edec66ca36ac70d2df8e9eb6da327dc
Aaron Long <aaron.long.c@gmail.com>
2025-02-10 23:02:26 -0500

    refactor: Import position data

--- src/resume/resume-base.tsx
+++ src/resume/resume-base.tsx
@@ -1,35 +1,24 @@
 import { useEffect, useState } from "react";
 
+import { Position } from "./types";
+import { PositionCard } from "./position";
+
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
 
 import { fetchResume } from "./data";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles("dark", {
-    backgroundColor: "#1A2027",
-  }),
-}));
-
 export function Resume() {
-  const [positions, setPositions] = useState([]);
+  const [positions, setPositions] = useState<Position[]>([]);
   useEffect(() => {
     fetchResume().then((data) => setPositions(data.positions));
   });
+
   return (
     <Box sx={{ width: "100%" }}>
       <Stack spacing={2}>
         {positions.map((position: any) => (
-          <Item key={position.company}>
-            {position.company} - {position.role}
-          </Item>
+          <PositionCard key={position.company} {...position} />
         ))}
       </Stack>
     </Box>
