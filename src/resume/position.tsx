4011b0c2009b9e7fdbf953dd393bd35c3aaecc7e
Aaron Long <aaron.long.c@gmail.com>
2025-02-14 20:06:12 -0500

    feat: Iterate on chips

--- src/resume/position.tsx
+++ src/resume/position.tsx
@@ -1,17 +1,61 @@
-import * as React from "react";
-
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Chip from "@mui/material/Chip";
 import Typography from "@mui/material/Typography";
+
+import { parse, format } from "date-fns";
+
+/**
+ * Probably overkill but provides flexibility if I want to change the format later
+ * @param date
+ * @returns
+ */
+const formatDate = (date: string) =>
+  format(parse(date, "yyyy-MM-dd", new Date()), "yyyy-MM");
+
+const DateChip = ({
+  date,
+  identifier,
+}: {
+  date: string;
+  identifier: string;
+}) => {
+  console.log("render");
+  const formatted = formatDate(date);
+  // TODO: Make identifier by theme
+  // https://mui.com/material-ui/customization/creating-themed-components/
+  return (
+    <Chip
+      color="secondary"
+      size="small"
+      // label={formatted}
+      label={
+        <>
+          <span>{identifier}: </span>
+          {formatted}
+        </>
+      }
+    />
+  );
+};
 
 interface PositionProps {
   role: string;
   company: string;
+  startDate: string;
+  endDate: string;
   keyResults: string[];
 }
 
-export function PositionCard({ company, role, keyResults }: PositionProps) {
+// https://mui.com/material-ui/customization/typography/#adding-amp-disabling-variants
+export function PositionCard({
+  company,
+  role,
+  keyResults,
+  startDate,
+  endDate,
+}: PositionProps) {
   return (
     <Box sx={{ minWidth: 275 }}>
       <Card variant="outlined">
@@ -21,6 +65,9 @@ export function PositionCard({ company, role, keyResults }: PositionProps) {
             sx={{ color: "text.secondary", fontSize: 14 }}
           >
             {company} - {role}
+            <br />
+            <DateChip date={startDate} identifier="start" />
+            {endDate && <DateChip date={endDate} identifier="end" />}
           </Typography>
           <Typography variant="body2" align="left">
             <ul>
