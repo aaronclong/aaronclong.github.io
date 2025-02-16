2dc5768d896ae8cac20b6e6b1cd9d8a537a28d65
Aaron Long <aaron.long.c@gmail.com>
2025-02-16 18:51:18 -0500

    fix(icons): Fix GCP & Add Aws

--- src/skill-icon-map.ts
+++ src/skill-icon-map.ts
@@ -4,11 +4,12 @@ import CSharpIcon from "devicon/icons/csharp/csharp-original.svg?url";
 import CloudRunIcon from "devicon/icons/googlecloud/googlecloud-original.svg?url";
 import DockerIcon from "devicon/icons/docker/docker-original.svg?url";
 // import EcsIcon from "devicon/icons/amazonwebservices/amazonwebservices-original.svg?url";
-import GcpIcon from "devicon/icons/google/google-original.svg?url";
+import GcpIcon from "devicon/icons/googlecloud/googlecloud-original-wordmark.svg?url";
 import JavaIcon from "devicon/icons/java/java-original.svg?url";
 import JavaScriptIcon from "devicon/icons/javascript/javascript-original.svg?url";
 import KubernetesIcon from "devicon/icons/kubernetes/kubernetes-original.svg?url";
 // import LambdaIcon from "devicon/icons/aws/aws-original.svg?url";
+import AWSLambdaIcon from "aws-icons/icons/architecture-service/AWSLambda.svg?url";
 import PythonIcon from "devicon/icons/python/python-original.svg?url";
 import ReactIcon from "devicon/icons/react/react-original.svg?url";
 import SqlIcon from "devicon/icons/mysql/mysql-original.svg?url";
@@ -50,7 +51,7 @@ export const skillIconMap = Object.freeze({
   [SkillId.docker]: DockerIcon,
   [SkillId.cloudrun]: CloudRunIcon,
   [SkillId.kubernetes]: KubernetesIcon,
-  [SkillId.lambda]: AwsIcon, //LambdaIcon,
+  [SkillId.lambda]: AWSLambdaIcon, //LambdaIcon,
   [SkillId.ecs]: AwsIcon, //EcsIcon,
   [SkillId.aws]: AwsIcon,
   [SkillId.gcp]: GcpIcon,
