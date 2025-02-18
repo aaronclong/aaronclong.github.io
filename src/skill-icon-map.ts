ff92e7d65036ae818a1f6052da2340b228e0a0c6
Aaron Long <aaron.long.c@gmail.com>
2025-02-17 23:15:12 -0500

    feat: Iterate on skill badges

--- src/skill-icon-map.ts
+++ src/skill-icon-map.ts
@@ -1,14 +1,14 @@
 import AngularIcon from "devicon/icons/angular/angular-original.svg?url";
 import AwsIcon from "devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg?url";
 import CSharpIcon from "devicon/icons/csharp/csharp-original.svg?url";
-import CloudRunIcon from "devicon/icons/googlecloud/googlecloud-original.svg?url";
+import CloudRunIcon from "./assets/icons/gcp/cloud_run.svg";
+import GcpFunctionIcon from "./assets/icons/gcp/cloud_functions.svg";
 import DockerIcon from "devicon/icons/docker/docker-original.svg?url";
-// import EcsIcon from "devicon/icons/amazonwebservices/amazonwebservices-original.svg?url";
+import AwsEcsIcon from "aws-icons/icons/architecture-service/AmazonECSAnywhere.svg?url";
 import GcpIcon from "devicon/icons/googlecloud/googlecloud-original-wordmark.svg?url";
 import JavaIcon from "devicon/icons/java/java-original.svg?url";
 import JavaScriptIcon from "devicon/icons/javascript/javascript-original.svg?url";
 import KubernetesIcon from "devicon/icons/kubernetes/kubernetes-original.svg?url";
-// import LambdaIcon from "devicon/icons/aws/aws-original.svg?url";
 import AWSLambdaIcon from "aws-icons/icons/architecture-service/AWSLambda.svg?url";
 import PandasIcon from "./assets/icons/pandas.svg?url";
 import PythonIcon from "devicon/icons/python/python-original.svg?url";
@@ -37,6 +37,7 @@ export enum SkillId {
   kubernetes = "k8s",
   lambda = "lambda",
   ecs = "ecs",
+  gcf = "gcf",
   aws = "aws",
   gcp = "gcp",
 }
@@ -56,7 +57,8 @@ export const skillIconMap = Object.freeze({
   [SkillId.cloudrun]: CloudRunIcon,
   [SkillId.kubernetes]: KubernetesIcon,
   [SkillId.lambda]: AWSLambdaIcon, //LambdaIcon,
-  [SkillId.ecs]: AwsIcon, //EcsIcon,
+  [SkillId.ecs]: AwsEcsIcon, //EcsIcon,
+  [SkillId.gcf]: GcpFunctionIcon,
   [SkillId.aws]: AwsIcon,
   [SkillId.gcp]: GcpIcon,
 });
