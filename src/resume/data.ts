27e06237f3ea7f1a310c3a141490cb89425f2c69
Aaron Long <aaron.long.c@gmail.com>
2025-02-20 20:20:15 -0500

    feat: Start data structures

--- src/resume/data.ts
+++ src/resume/data.ts
@@ -1,6 +1,19 @@
-import { Resume } from "./types";
+import type { Resume, Skills, SkillType } from "./types";
 
 export async function fetchResume(): Promise<Resume> {
   const response = await fetch("resume.json");
   return await response.json();
+}
+
+export type SkillMap = Map<string, SkillType>;
+export function createSkillById(skills: Skills): SkillMap {
+  const values: [string, SkillType][] = [];
+  const appendToValues = (value: SkillType) => values.push([value.id, value]);
+
+  skills.languages.forEach(appendToValues);
+  skills.cloudProviders.forEach(appendToValues);
+  skills.frameworks.forEach(appendToValues);
+  skills.infra.forEach(appendToValues);
+
+  return new Map(values);
 }
