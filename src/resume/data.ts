4d049557a9e038dffadebd174bf0ad0c77aee894
Aaron Long <aaron.long.c@gmail.com>
2025-02-22 17:21:04 -0500

    style: Remove unused function

--- src/resume/data.ts
+++ src/resume/data.ts
@@ -6,21 +6,10 @@ export async function fetchResume(): Promise<Resume> {
 }
 
 export type SkillMap = Map<string, SkillType>;
-export function createSkillById(skills: Skills): SkillMap {
-  const values: [string, SkillType][] = [];
-  const appendToValues = (value: SkillType) => values.push([value.id, value]);
-
-  skills.languages.forEach(appendToValues);
-  skills.cloudProviders.forEach(appendToValues);
-  skills.frameworks.forEach(appendToValues);
-  skills.infra.forEach(appendToValues);
-
-  return new Map(values);
-}
 
 export class SkillDictionary {
   private constructor(
-    private readonly skillMap: Map<string, SkillType>,
+    private readonly skillMap: SkillMap,
     private readonly idByName: Map<string, string>
   ) {}
 
