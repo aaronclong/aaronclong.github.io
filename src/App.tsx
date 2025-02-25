460437cc9d875e1c48747dca905e2b56186e0288
Aaron Long <aaron.long.c@gmail.com>
2025-02-25 00:30:14 -0500

    fix: Force light mode for all context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { ThemeProvider, createTheme, useColorScheme } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 import { Resume } from "./resume";
 import "./App.css";
 import { ConsentModal } from "./analytics/consent";
 
-export function App() {
-  const theme = createTheme({
-    colorSchemes: {
-      dark: false,
-    },
-  });
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
 
+export function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Resume />
       <ConsentModal />
     </ThemeProvider>
