597fd29d758e2f136eb963e6a0776ce5eddeca17
Aaron Long <aaron.long.c@gmail.com>
2025-02-25 00:14:03 -0500

    feat: Begin to define theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { ThemeProvider, createTheme, useColorScheme } from "@mui/material";
+
 import { Resume } from "./resume";
 import "./App.css";
 import { ConsentModal } from "./analytics/consent";
 
 export function App() {
+  const theme = createTheme({
+    colorSchemes: {
+      dark: false,
+    },
+  });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Resume />
       <ConsentModal />
-    </>
+    </ThemeProvider>
   );
 }
